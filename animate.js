caeef730cb1068cb8baad596b19fbd9a046a1fac
eliechoi2 <102823232+eliechoi2@users.noreply.github.com>
2022-05-04 22:43:06 -0400

    a

--- animate.js
+++ animate.js
@@ -16,5 +16,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
+  setTimeout(showSlides, 4000); 
 }
